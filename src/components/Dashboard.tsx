5b43f803e7b5452ccfc458dc1d6d1e3b653eec2f
koebe1 <koe.be.pay@web.de>
2021-12-03 16:49:28 +0100

    added nav with icons to the dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { BiGitRepoForked } from "react-icons/bi";
+import { GiProgression } from "react-icons/gi";
 import TreeChart from "./treeChart/TreeChart";
 
 export const Dashboard = ({ stories }) => {
   return (
-    <>
-      <div className="content-container">
-        <div className="tree flex-center" style={{ marginRight: "20px" }}>
-          <TreeChart stories={stories} />
-        </div>
+    <div className="content-container">
+      <nav style={{ padding: "10px" }}>
+        <BiGitRepoForked
+          style={{
+            fontSize: "30px",
+            color: "#d3d3d3",
+            transform: "rotate(180deg)",
+            border: "1px solid #d3d3d3",
+            borderRadius: "4px",
+            marginRight: "5px",
+          }}
+        />
+        <GiProgression
+          style={{
+            fontSize: "30px",
+            color: "#777",
+            border: "1px solid #d3d3d3",
+            borderRadius: "4px",
+          }}
+        />
+      </nav>
+      <div className="tree flex-center" style={{ marginRight: "20px" }}>
+        <TreeChart stories={stories} />
       </div>
-    </>
+    </div>
   );
 };
