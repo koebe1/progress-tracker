6779d10e33dcf438bd555333c17de2095c59c2f8
koebe1 <koe.be.pay@web.de>
2021-12-03 15:44:34 +0100

    added d3 tree overview of stories

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,10 +3,12 @@ import TreeChart from "./TreeChart";
 
 export const Dashboard = ({ stories }) => {
   return (
-    <div className="content-container">
-      <div className="tree flex-center">
-        <TreeChart stories={stories} />
+    <>
+      <div className="content-container">
+        <div className="tree flex-center">
+          <TreeChart stories={stories} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/components/TreeChart.tsx
+++ src/components/TreeChart.tsx
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
-import { select, hierarchy } from "d3";
+import { select, hierarchy, tree, linkHorizontal } from "d3";
 import useResizeObserver from "./useResizeObserver";
 
 // maps stories to data format for d3
 // -> data = {name: "name", children: {...}}
 const mapStories = (stories) => {
   const data = {
-    name: "Your Stories",
+    name: "Stories:",
     children: [],
   };
 
@@ -27,26 +27,118 @@ const mapStories = (stories) => {
   return data;
 };
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 const TreeChart = ({ stories }) => {
   const [data, setData] = useState({});
 
   const svgRef = useRef();
-  const wrappedRef = useRef();
-  const dimensions = useResizeObserver(wrappedRef);
+  const wrapperRef: any = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
 
+  // listener to map stories to the needed d3 data structure
   useEffect(() => {
     setData(mapStories(stories));
   }, [stories]);
 
+  // we save data to see if it changed
+  const previouslyRenderedData = usePrevious(data);
+
   useEffect(() => {
     const svg = select(svgRef.current);
-    if (!dimensions) return;
 
+    // use dimensions from useResizeObserver,
+    // but use getBoundingClientRect on initial render
+    // (dimensions are null for the first render)
+    const { width, height } =
+      dimensions || wrapperRef.current.getBoundingClientRect();
+
+    // transform hierarchical data
     const root = hierarchy(data);
-  }, [data, dimensions]);
+    const treeLayout = tree().size([height, width]);
+
+    const linkGenerator = linkHorizontal()
+      .x((link) => link.y)
+      .y((link) => link.x);
+
+    // enrich hierarchical data with coordinates
+    treeLayout(root);
+
+    console.warn("descendants", root.descendants());
+    console.warn("links", root.links());
+
+    // nodes
+    svg
+      .selectAll(".node")
+      .data(root.descendants())
+      .join((enter) => enter.append("circle").attr("opacity", 0))
+      .attr("class", "node")
+      .attr("cx", (node) => node.y)
+      .attr("cy", (node) => node.x)
+      .attr("r", 4)
+      .transition()
+      .duration(500)
+      .delay((node) => node.depth * 300)
+      .attr("opacity", 1);
+
+    // links
+    const enteringAndUpdatingLinks = svg
+      .selectAll(".link")
+      .data(root.links())
+      .join("path")
+      .attr("class", "link")
+      .attr("d", linkGenerator)
+      .attr("stroke-dasharray", function () {
+        const length = this.getTotalLength();
+        return `${length} ${length}`;
+      })
+      .attr("stroke", "black")
+      .attr("fill", "none")
+      .attr("opacity", 1);
+
+    if (data !== previouslyRenderedData) {
+      enteringAndUpdatingLinks
+        .attr("stroke-dashoffset", function () {
+          return this.getTotalLength();
+        })
+        .transition()
+        .duration(500)
+        .delay((link) => link.source.depth * 500)
+        .attr("stroke-dashoffset", 0);
+    }
+
+    // labels
+    svg
+      .selectAll(".label")
+      .data(root.descendants())
+      .join((enter) => enter.append("text").attr("opacity", 0))
+      .attr("class", "label")
+      .attr("x", (node) => node.y)
+      .attr("y", (node) => node.x - 12)
+      .attr("text-anchor", "middle")
+      .attr("font-size", 4)
+      .text((node) => node.data.name)
+      .transition()
+      .duration(500)
+      .delay((node) => node.depth * 300)
+      .attr("opacity", 1);
+  }, [data, dimensions, previouslyRenderedData]);
+
   return (
-    <div ref={wrappedRef}>
-      <svg ref={svgRef}></svg>
+    <div
+      ref={wrapperRef}
+      style={{
+        marginTop: "30px",
+        height: "50vh",
+      }}
+    >
+      <svg style={{ overflow: "visible" }} ref={svgRef}></svg>
     </div>
   );
 };
