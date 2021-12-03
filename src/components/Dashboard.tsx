2f612e3667e16990cb35143164c7e6be0b328624
koebe1 <koe.be.pay@web.de>
2021-12-03 12:20:20 +0100

    added mapping function to map stories to correct data format for d3

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import TreeChart from "./TreeChart";
 
 export const Dashboard = ({ stories }) => {
   return (
     <div className="content-container">
-      <div className="tree flex-center">{JSON.stringify(stories)}</div>
+      <div className="tree flex-center">
+        <TreeChart stories={stories} />
+      </div>
     </div>
   );
 };
--- src/components/TreeChart.tsx
+++ src/components/TreeChart.tsx
@@ -1,15 +1,48 @@
-import React, { useRef, useEffect } from "react";
-import { select } from "d3";
+import React, { useRef, useEffect, useState } from "react";
+import { select, hierarchy } from "d3";
 import useResizeObserver from "./useResizeObserver";
 
-const TreeChart = ({ data }) => {
+// maps stories to data format for d3
+// -> data = {name: "name", children: {...}}
+const mapStories = (stories) => {
+  const data = {
+    name: "Your Stories",
+    children: [],
+  };
+
+  for (let story in stories) {
+    // case: story has a substory
+    if (Object.keys(stories[story].subStories).length > 0) {
+      const subStories = [];
+      // loop through substories
+      for (let subStory in stories[story].subStories) {
+        subStories.push({ name: subStory });
+      }
+      data.children.push({ name: story, children: subStories });
+      // case story jas no substory
+    } else {
+      data.children.push({ name: story });
+    }
+  }
+  return data;
+};
+
+const TreeChart = ({ stories }) => {
+  const [data, setData] = useState({});
+
   const svgRef = useRef();
   const wrappedRef = useRef();
   const dimensions = useResizeObserver(wrappedRef);
 
   useEffect(() => {
+    setData(mapStories(stories));
+  }, [stories]);
+
+  useEffect(() => {
     const svg = select(svgRef.current);
     if (!dimensions) return;
+
+    const root = hierarchy(data);
   }, [data, dimensions]);
   return (
     <div ref={wrappedRef}>
@@ -19,3 +52,17 @@ const TreeChart = ({ data }) => {
 };
 
 export default TreeChart;
+
+// const data = {
+//     name: "Your Stories",
+//     children: [
+//         {
+//             name: "story1",
+//             children: [
+//                 {
+//                     name: "subStory1"
+//                 }
+//             ]
+//     }
+//  ]
+// }
