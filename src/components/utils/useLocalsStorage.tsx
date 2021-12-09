98452daeb0d1841d825a1d3d99117bf43b911da9
koebe1 <koe.be.pay@web.de>
2021-12-09 16:27:13 +0100

    refactored useLocalStorage.tsx

--- src/components/utils/useLocalsStorage.tsx
+++ src/components/utils/useLocalsStorage.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-export const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = (key: string, initialValue: string | object) => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-      const item = window.localStorage.getItem(key);
+      const item: string | null = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
